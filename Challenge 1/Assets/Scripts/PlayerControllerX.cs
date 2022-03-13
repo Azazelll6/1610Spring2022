ad0d83b6c9c9df9d461db2ff371f7a3e1da72f25
Zac <10719862@uvu.edu>
2022-03-13 17:05:53 -0600

    Challenge 1 Airplane sim
    All complete including prop spin

---
+++ Challenge 1/Assets/Scripts/PlayerControllerX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float speed = 0.4f;
+    private float rotationSpeed = 150;
+    private float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * rotationSpeed *verticalInput * Time.deltaTime);
+    }
+}
