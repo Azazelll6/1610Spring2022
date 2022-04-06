29029eadf8eb76100e68ae565d47e81cc8b116e3
Zac <10719862@uvu.edu>
2022-04-05 22:05:32 -0600

    Finished Prototype5
    Prototype 5 commit

--- Prototype5/Assets/Scripts/Target.cs
+++ Prototype5/Assets/Scripts/Target.cs
@@ -1,26 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Target : MonoBehaviour
 {
+    private GameManager gameManager;
     private Rigidbody targetRb;
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -2;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+    
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
         transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
+
     void Update()
     {
         
     }
+
     Vector3 RandomForce()
     {
         return Vector3.up * Random.Range(minSpeed, maxSpeed);
@@ -35,9 +44,19 @@ public class Target : MonoBehaviour
     }
     private void OnMouseDown()
     {
-        Destroy(gameObject);   
-    }    private void OnTriggerEnter(Collider other)
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);  
+        }
+    }    
+    private void OnTriggerEnter(Collider other)
     {
-        Destroy(gameObject);   
+        Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
     }
 }
